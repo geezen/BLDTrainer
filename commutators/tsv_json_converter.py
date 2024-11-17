553b5a451c98a19afe576875d1326590b8521411
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2024-11-17 10:20:42 +0100

    working tsv to js converter

--- commutators/tsv_json_converter.py
+++ commutators/tsv_json_converter.py
@@ -2,25 +2,40 @@ import csv
 import json
 
 FILE_PATHS = (
-    ("edgeComms", "Dylan's 3-Style Learning Sheet - UF Comms.tsv"),
-    ("edgeTypes", "Dylan's 3-Style Learning Sheet - UF Types.tsv"),
-    ("cornerComms", "Dylan's 3-Style Learning Sheet - UFR Comms.tsv"),
-    ("cornerTypes", "Dylan's 3-Style Learning Sheet - UFR Types.tsv")
+    ("EDGE_COMMS", "Dylan's 3-Style Learning Sheet - UF Comms.tsv"),
+    ("EDGE_TYPE", "Dylan's 3-Style Learning Sheet - UF Types.tsv"),
+    ("CORNER_COMMS", "Dylan's 3-Style Learning Sheet - UFR Comms.tsv"),
+    ("CORNER_TYPES", "Dylan's 3-Style Learning Sheet - UFR Types.tsv")
 )
+OUTPUT_PATH = "commutators.js"
 
+output = ""
 
-two_dimensional_array = []
-with open("Dylan's 3-Style Learning Sheet - UF Comms.tsv", 'r') as file:
-    tsv_reader = csv.reader(file, delimiter='\t')
-    for row in tsv_reader:
-        two_dimensional_array.append(row)
+# process each tsv file
+for label, path in FILE_PATHS:
+    raw_array = []
+    temp_dict = {}
 
-my_dict = {}
-for i in range(1, len(two_dimensional_array)):
-    for j in range(1, len(two_dimensional_array[0])):
-        first_letter = two_dimensional_array[0][j][0]
-        second_letter = two_dimensional_array[i][0][0]
-        my_dict[f"{first_letter}{second_letter}"] = two_dimensional_array[i][j]
+    # read tsv file to an array
+    with open(path, 'r') as file:
+        tsv_reader = csv.reader(file, delimiter='\t')
+        for row in tsv_reader:
+            raw_array.append(row)
 
-print(json.dumps(my_dict, indent=4))
-        
+    # parse the array into a dict
+    for row in range(1, len(raw_array)):
+        for col in range(1, len(raw_array[0])):
+            first_letter = raw_array[0][col][0]
+            second_letter = raw_array[row][0][0]
+            commutator = raw_array[row][col]
+            temp_dict[f"{first_letter}{second_letter}"] = commutator
+    
+    # format output
+    json_string = json.dumps(temp_dict, indent=4)
+    output += f"const {label} = {json_string};\n"
+
+# write output to file
+with open(OUTPUT_PATH, "w") as js_file:
+    js_file.write(output)
+
+print(f"Processed data written to {OUTPUT_PATH}")
